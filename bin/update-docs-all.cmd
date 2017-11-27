800d28570c03f1279e301c0ee65f9b29473e8901
Michele Locati <michele@locati.it>
2017-11-27 08:45:36 +0100

    New PHP-CS-Fixer versions: 2.2.12, 2.8.3

--- bin/update-docs-all.cmd
+++ bin/update-docs-all.cmd
@@ -1,10 +1,11 @@
 @echo off
 setlocal
-del "%~dp0../docs/js/php-cs-fixer-data-*.json" >NUL 2>NUL
-del "%~dp0../docs/js/php-cs-fixer-versions.json" >NUL 2>NUL
+del "%~dp0..\docs\js\php-cs-fixer-data-*.json" >NUL 2>NUL
+del "%~dp0..\docs\js\php-cs-fixer-versions.json" >NUL 2>NUL
 for /f "tokens=*" %%a in (%~dp0versions.txt) do (
     php "%~dp0update-docs.php" "%%a"
     if errorlevel 1 (
     	exit /b %ERRORLEVEL%
     )
 )
+composer --working-dir="%~dp0.." require friendsofphp/php-cs-fixer:2.* >NUL 2>NUL
