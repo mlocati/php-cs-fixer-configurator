e817fd714b17d2b32b538c56e14da162a6d4d50c
Clemens Krack <c.krack@wikando.de>
2020-08-27 01:01:49 +0200

    feat: Add ECS exporter

--- web/Export/Exporters.ts
+++ web/Export/Exporters.ts
@@ -1,11 +1,13 @@
 import ExporterInterface from "./ExporterInterface";
 import JsonExporter from "./JsonExporter";
 import PhpCsExporter from "./PhpCsExporter";
+import PhpECSExporter from "./PhpECSExporter";
 import StyleCIExporter from "./StyleCIExporter";
 import YamlExporter from "./YamlExporter";
 
 const Exporters: ExporterInterface[] = [
     new PhpCsExporter(),
+    new PhpECSExporter(),
     new JsonExporter(),
     new YamlExporter(),
     new StyleCIExporter(),
---
+++ web/Export/PhpECSExporter.ts
@@ -0,0 +1,103 @@
+import ExporterInterface, { RenderOptions } from "./ExporterInterface";
+import { SerializedConfigurationInterface } from "../Configuration";
+import { toPhp } from "../Utils";
+
+export default class PhpECSExporter implements ExporterInterface {
+
+    readonly handle: string = 'php-ecs';
+
+    readonly name: string = 'ecs.php';
+
+    readonly language: string = 'php';
+
+    readonly supportConfiguringWhitespace: boolean = true;
+
+    readonly supportFixerDescriptions: boolean = true;
+
+    /**
+     * Generate the text representing the configuration
+     * @param version
+     * @param configuration
+     * @param keepMetadata
+     */
+    render(configuration: SerializedConfigurationInterface, options: RenderOptions): string {
+        const INDENT = '    ';
+        const LINE_ENDING = '\n';
+        let lines: string[] = [
+            '<?php',
+            'declare(strict_types=1);',
+            '',
+            '/*',
+            ' * This document has been generated with',
+            ' * https://mlocati.github.io/php-cs-fixer-configurator/#version:' + configuration.version + '|configurator',
+            ' * you can change this configuration by importing this file.',
+            ' */',
+            '',
+            'use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;',
+            'use Symplify\EasyCodingStandard\Configuration\Option;',
+            '',
+            'return static function (ContainerConfigurator $containerConfigurator): void {',
+            INDENT + '$parameters = $containerConfigurator->parameters();',
+        ];
+        if (configuration.risky) {
+            lines.push(INDENT + '// risky are allowed, though there is no setting in ECS.');
+        }
+        if (configuration.whitespace !== undefined) {
+            if (configuration.whitespace.indent !== undefined) {
+                lines.push(INDENT + '$parameters->set(Option::INDENTATION,' + toPhp(configuration.whitespace.indent) + ')');
+            }
+            if (configuration.whitespace.lineEnding !== undefined) {
+                lines.push(INDENT + '$parameters->set(Option::LINE_ENDING,' + toPhp(configuration.whitespace.lineEnding) + ')');
+            }
+        }
+
+        lines.push(INDENT + '$parameters->set(Option::PATHS, [');
+        lines.push(INDENT + INDENT + '__DIR__ . \'/src\',');
+        lines.push(INDENT + INDENT + '__DIR__ . \'/tests\',');
+        lines.push(INDENT + ']);');
+        lines.push('');
+
+        lines.push(INDENT + '$parameters->set(Option::EXCLUDE_PATHS, [');
+        lines.push(INDENT + INDENT + '__DIR__ . \'/vendor\',');
+        lines.push(INDENT + ']);');
+        lines.push('');
+
+        if (configuration.fixerSets !== undefined) {
+            lines.push(INDENT + '$parameters->set(Option::SETS, [');
+            lines.push(INDENT + INDENT + '// must be done manually, as names differ');
+            configuration.fixerSets.forEach((fixerSetName: string): void => {
+                if (fixerSetName.charAt(0) === '-') {
+                    lines.push(INDENT + INDENT + '// exclude: SetList::' + fixerSetName.substr(1) + ',');
+                } else {
+                    lines.push(INDENT + INDENT + '// include SetList::' + fixerSetName.substr(2) + ',');
+                }
+            });
+            lines.push(INDENT + ']);');
+            lines.push('');
+        }
+        if (configuration.fixers !== undefined) {
+            lines.push(INDENT + '$services = $containerConfigurator->services();');
+            Object.keys(configuration.fixers).forEach((fixerName: string): void => {
+                const fixer = options.version.getFixerByName(fixerName);
+                if (fixer === null) {
+                    return;
+                }
+                if (options.exportFixerDescriptions) {
+                    let comment = fixer.summary.length === 0 ? fixer.description : fixer.summary;
+                    if (comment.length > 0) {
+                        lines.push(INDENT + '// ' + comment.replace(/\r\n?/g, ' ').replace(/\?>/g, '? >'));
+                    }
+                }
+                const fixerConfiguration = (<{ [fixerName: string]: boolean | object }>configuration.fixers)[fixerName];
+                if (typeof fixerConfiguration === 'boolean') {
+                    lines.push(INDENT + '$services->set(' + fixer.fullClassName + '::class);');
+                } else {
+                    lines.push(INDENT + '$services->set(' + fixer.fullClassName + '::class)');
+                    lines.push(INDENT + INDENT + '->call(\'configure\', [' + toPhp(fixerConfiguration) + ']);');
+                }
+            });
+        }
+        lines.push('\};\n');
+        return lines.join(LINE_ENDING);
+    }
+}
