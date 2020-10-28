mlocati/php-cs-fixer-configurator

commit 1d64d486b05f726c255047f0524e12dca1afbd0c

feat(PhpEcsExporter): add optional imports
Adds option to import classes.

web/components/Export.vue
@@ -49,8 +49,12 @@
                 <b-form-checkbox v-model="expandFixerSets">Expand presets</b-form-checkbox>
                 <b-form-checkbox
                     v-model="exportFixerDescriptions"
-                    v-bind:disabled="!exporter.supportFixerDescriptions"
+                    v-bind:disabled="!exporter.supportImportFixerClasses"
                 >Export fixer descriptions</b-form-checkbox>
+                <b-form-checkbox
+                    v-model="importFixerClasses"
+                    v-bind:disabled="!exporter.supportFixerDescriptions"
+                >Import fixer classes</b-form-checkbox>
             </div>
         </b-form-group>
 
@@ -112,6 +116,7 @@ export default Vue.extend({
             outputError: null,
             expandFixerSets: false,
             exportFixerDescriptions: false,
+            importFixerClasses: false
         };
     },
     mounted: function() {
@@ -143,6 +148,9 @@ export default Vue.extend({
         exportFixerDescriptions: function() {
             this.refreshOutput(false);
         },
+        importFixerClasses: function() {
+            this.refreshOutput(false);
+        },
     },
     methods: {
         copyToClipboard: function() {
@@ -163,6 +171,7 @@ export default Vue.extend({
                 this.output = this.exporter.render(serializedConfiguration, {
                     version: this.configuration.version,
                     exportFixerDescriptions: this.exportFixerDescriptions,
+                    importFixerClasses: this.importFixerClasses,
                 });
                 this.outputError = null;
             } catch (e) {

web/Export/ExporterInterface.ts
@@ -3,7 +3,8 @@ import Version from "../Version";
 
 export interface RenderOptions {
     readonly version: Version;
-    readonly exportFixerDescriptions: boolean,
+    readonly exportFixerDescriptions: boolean;
+    readonly importFixerClasses?: boolean;
 }
 
 interface ExporterInterface {
@@ -31,6 +32,11 @@ interface ExporterInterface {
      * Does this exporter support exporting fixer descriptions?
      */
     readonly supportFixerDescriptions: boolean;
+
+    /**
+     * Does this exporter support importing fixer classes?
+     */
+    readonly supportImportFixerClasses?: boolean;
 
     /**
      * Generate the text representing the configuration

web/Export/PhpECSExporter.ts
@@ -1,6 +1,6 @@
 import ExporterInterface, { RenderOptions } from "./ExporterInterface";
 import { SerializedConfigurationInterface } from "../Configuration";
-import { toPhp, setNameToConst } from "../Utils";
+import {toPhp, setNameToConst } from "../Utils";
 
 export default class PhpECSExporter implements ExporterInterface {
 
@@ -13,6 +13,8 @@ export default class PhpECSExporter implements ExporterInterface {
     readonly supportConfiguringWhitespace: boolean = true;
 
     readonly supportFixerDescriptions: boolean = true;
+
+    readonly supportImportFixerClasses: boolean = true;
 
     /**
      * Generate the text representing the configuration
@@ -30,12 +32,21 @@ export default class PhpECSExporter implements ExporterInterface {
             ' * you can change this configuration by importing this file.',
             ' */',
             '',
-            'use Symfony\\Component\\DependencyInjection\\Loader\\Configurator\\ContainerConfigurator;',
-            'use Symplify\\EasyCodingStandard\\Configuration\\Option;',
-            '',
-            'return static function (ContainerConfigurator $containerConfigurator): void {',
-            INDENT + '$parameters = $containerConfigurator->parameters();',
         ];
+
+        if (options.importFixerClasses && configuration.fixers !== undefined) {
+            let importFixers = PhpECSExporter.getImports(Object.keys(configuration.fixers), options);
+            importFixers.forEach((fixerName: string): void => {
+                lines.push('use ' + fixerName + ';');
+            });
+        }
+
+        lines.push('use Symfony\\Component\\DependencyInjection\\Loader\\Configurator\\ContainerConfigurator;');
+        lines.push('use Symplify\\EasyCodingStandard\\Configuration\\Option;');
+        lines.push('');
+        lines.push('return static function (ContainerConfigurator $containerConfigurator): void {');
+        lines.push(INDENT + '$parameters = $containerConfigurator->parameters();');
+
         if (configuration.risky) {
             lines.push(INDENT + '// risky are allowed, though there is no setting in ECS.');
         }
@@ -85,15 +96,32 @@ export default class PhpECSExporter implements ExporterInterface {
                     }
                 }
                 const fixerConfiguration = (<{ [fixerName: string]: boolean | object }>configuration.fixers)[fixerName];
+                let fixerUsedName = options.importFixerClasses ? PhpECSExporter.getImportedName(fixer.fullClassName) : fixer.fullClassName;
                 if (typeof fixerConfiguration === 'boolean') {
-                    lines.push(INDENT + '$services->set(' + fixer.fullClassName + '::class);');
+                    lines.push(INDENT + '$services->set(' + fixerUsedName + '::class);');
                 } else {
-                    lines.push(INDENT + '$services->set(' + fixer.fullClassName + '::class)');
-                    lines.push(INDENT + INDENT + '->call(\'configure\', [' + toPhp(fixerConfiguration) + ']);');
+                    lines.push(INDENT + '$services->set(' + fixerUsedName + '::class)');
+                    lines.push(INDENT + '         ' + '->call(\'configure\', [' + toPhp(fixerConfiguration) + ']);');
                 }
             });
         }
         lines.push('\};\n');
         return lines.join(LINE_ENDING);
+    }
+
+    private static getImportedName(classname: string): string {
+        let namespaces: string[] = classname.split('\\');
+        return namespaces[namespaces.length - 1];
+    }
+
+    private static getImports(fixers: any, options: any): any {
+        let imports: string[] = [];
+        fixers.forEach((fixerName: string): void => {
+            const fixer = options.version.getFixerByName(fixerName);
+            if (fixer !== null) {
+                imports.push(fixer.fullClassName);
+            }
+        });
+        return imports.sort();
     }
 }

commit 7905c9c6b25cf452ded0415279f21a35185532d6

Fix bug introduced in 1d64d486b05f726c255047f0524e12dca1afbd0c

web/components/Export.vue
@@ -49,11 +49,11 @@
                 <b-form-checkbox v-model="expandFixerSets">Expand presets</b-form-checkbox>
                 <b-form-checkbox
                     v-model="exportFixerDescriptions"
-                    v-bind:disabled="!exporter.supportImportFixerClasses"
+                    v-bind:disabled="!exporter.supportFixerDescriptions"
                 >Export fixer descriptions</b-form-checkbox>
                 <b-form-checkbox
                     v-model="importFixerClasses"
-                    v-bind:disabled="!exporter.supportFixerDescriptions"
+                    v-bind:disabled="!exporter.supportImportFixerClasses"
                 >Import fixer classes</b-form-checkbox>
             </div>
         </b-form-group>